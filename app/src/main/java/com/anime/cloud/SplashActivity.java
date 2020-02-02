f90f55f932b90e01f228b6a394df967514e23150
Mauro2888 <mauro.link39@gmail.com>
2020-02-02 21:13:05 +0100

    Added splash screen

---
+++ app/src/main/java/com/anime/cloud/SplashActivity.java
@@ -0,0 +1,42 @@
+package com.anime.cloud;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+
+import gr.net.maroulis.library.EasySplashScreen;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        EasySplashScreen splashScreen = new EasySplashScreen(SplashActivity.this)
+                .withFullScreen()
+                .withTargetActivity(MainActivity.class)
+                .withSplashTimeOut(5000)
+                .withBackgroundColor(Color.parseColor("#000000"))
+                .withLogo(R.drawable.icon_anime);
+
+        View view = splashScreen.create();
+        setContentView(view);
+        hideSystemUI();
+    }
+
+    protected void hideSystemUI() {
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.KITKAT) {
+            getWindow().getDecorView()
+                    .setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                    );
+        }
+    }
+}
